68199ab0217ece73f866f0ad6ede1457fc4420bb
viethang <phanvietthang94@gmail.com>
2022-08-05 18:02:35 +0700

    add some libraries

---
+++ src/pages/Home/container/HomeContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@/common/components/button'
+
+const HomeContainer = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button />
+    </div>
+  )
+}
+
+export default HomeContainer
